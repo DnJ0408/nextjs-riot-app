551ab245d18b5c8ef6429c62a2e557814aaf33e4
DnJ0408 <oyj921205@gmail.com>
2025-03-12 22:33:43 +0900

    ✨ Update: Add the Item list #10

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,7 +1,37 @@
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+import { fetchVersions } from "@/utils/commonApi";
+import { fetchItemList } from "@/utils/serverApi";
+import Image from "next/image";
 
-export default page
+const BASE_URL = process.env.BASE_URL;
+
+const items = async () => {
+
+  const versions = await fetchVersions();
+  const {data} = await fetchItemList();
+  const items = Object.entries(data).map(([id, value]) => ({
+    id,
+    ...value,
+  }));
+
+  return (
+    <div>
+      <h1>Item List</h1>
+      <ul>
+      {items.map((item)=> 
+        <li key={item.id}>
+          <Image
+          src={`${BASE_URL}/cdn/${versions[0]}/img/item/${item.image.full}`}
+          alt={item.name}
+          width={100}
+          height={100}
+          />
+          {item.name}
+          {item.plaintext}
+        </li>
+      )}
+      </ul>
+    </div>
+  );
+};
+
+export default items;
