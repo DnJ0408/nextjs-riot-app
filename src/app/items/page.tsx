496be879464b0df89f4d7d90a73f838eeb09f947
DnJ0408 <oyj921205@gmail.com>
2025-03-12 23:40:00 +0900

    🎨 Style: Item cards #10

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -5,31 +5,36 @@ import Image from "next/image";
 const BASE_URL = process.env.BASE_URL;
 
 const items = async () => {
-
   const versions = await fetchVersions();
-  const {data} = await fetchItemList();
+  const { data } = await fetchItemList();
   const items = Object.entries(data).map(([id, value]) => ({
     id,
     ...value,
   }));
 
   return (
-    <div>
-      <h1>Item List</h1>
-      <ul>
-      {items.map((item)=> 
-        <li key={item.id}>
-          <Image
-          src={`${BASE_URL}/cdn/${versions[0]}/img/item/${item.image.full}`}
-          alt={item.name}
-          width={100}
-          height={100}
-          />
-          {item.name}
-          {item.plaintext}
-        </li>
-      )}
-      </ul>
+    <div className="w-full min-h-screen p-4 bg-gray-800 text-white">
+      <div className="container mx-auto">
+        <h1 className="text-2xl font-bold mb-4">Item List</h1>
+        <div className="flex flex-wrap justify-center gap-4">
+          {items.map((item) => (
+            <li
+              key={item.id}
+              className="border rounded-lg shadow-md p-4 flex flex-col items-center text-center w-40 h-60 bg-gray-900"
+            >
+              <Image
+                src={`${BASE_URL}/cdn/${versions[0]}/img/item/${item.image.full}`}
+                alt={item.name}
+                width={100}
+                height={100}
+                className="mb-2"
+              />
+              <h2 className="text-lg font-semibold">{item.name}</h2>
+              <p className="text-sm text-gray-500 overflow-hidden text-ellipsis line-clamp-3">{item.plaintext}</p>
+            </li>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
