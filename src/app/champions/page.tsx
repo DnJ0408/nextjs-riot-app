2af65a6ab5ba83ccfe5a70a6a27795186c3a9b3a
DnJ0408 <oyj921205@gmail.com>
2025-03-14 02:06:43 +0900

    🧹 Cleanup: Change double quotes to single quotes #12

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,6 +1,6 @@
-import ChampionCard from "@/components/champions/ChampionCard";
-import { fetchVersions } from "@/utils/commonApi";
-import { fetchChampionList } from "@/utils/serverApi";
+import ChampionCard from '@/components/champions/ChampionCard';
+import { fetchVersions } from '@/utils/commonApi';
+import { fetchChampionList } from '@/utils/serverApi';
 
 const champions = async () => {
   const versions = await fetchVersions();
@@ -9,10 +9,10 @@ const champions = async () => {
   const champions = Object.values(data);
 
   return (
-    <div className="w-full min-h-screen p-4 bg-gray-800 text-white">
-      <div className="container mx-auto">
-        <h1 className="text-2xl font-bold mb-4">Champion List</h1>
-        <div className="flex flex-wrap justify-center gap-4">
+    <div className='w-full min-h-screen p-4 bg-gray-800 text-white'>
+      <div className='container mx-auto'>
+        <h1 className='text-2xl font-bold mb-4'>Champion List</h1>
+        <div className='flex flex-wrap justify-center gap-4'>
           {champions.map((champion) => (
             <ChampionCard key={champion.id} champion={champion} versions={versions}  />
           ))}
