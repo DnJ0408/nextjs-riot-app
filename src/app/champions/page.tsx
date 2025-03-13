3d2922410392f997a93c4b96e9e757ce7b9152a7
DnJ0408 <oyj921205@gmail.com>
2025-03-14 02:00:54 +0900

    ♻️ Refactor: Modularising the Champion Card #12

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,9 +1,8 @@
+import ChampionCard from "@/components/champions/ChampionCard";
 import { fetchVersions } from "@/utils/commonApi";
 import { fetchChampionList } from "@/utils/serverApi";
-import Image from "next/image";
 
 const champions = async () => {
-  const BASE_URL = process.env.BASE_URL;
   const versions = await fetchVersions();
   const { data } = await fetchChampionList();
 
@@ -15,22 +14,7 @@ const champions = async () => {
         <h1 className="text-2xl font-bold mb-4">Champion List</h1>
         <div className="flex flex-wrap justify-center gap-4">
           {champions.map((champion) => (
-            <li
-              key={champion.id}
-              className="border rounded-lg shadow-md p-4 flex flex-col items-center text-center w-56 h-72 bg-gray-900 transition-all transform hover:scale-105 hover:text-red-600"
-            >
-              <Image
-                src={`${BASE_URL}/cdn/${versions[0]}/img/champion/${champion.image.full}`}
-                alt={champion.name}
-                width={120}
-                height={120}
-                className="mb-3 rounded-full"
-              />
-              <h2 className="text-2xl font-bold mt-4 mb-1">{champion.name}</h2>
-              <p className="text-lg text-gray-500 overflow-hidden text-ellipsis line-clamp-2">
-                {champion.title}
-              </p>
-            </li>
+            <ChampionCard key={champion.id} champion={champion} versions={versions}  />
           ))}
         </div>
       </div>
