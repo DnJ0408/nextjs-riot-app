40cd0c14fc2a24622aa4ffead4e20a57338da297
DnJ0408 <oyj921205@gmail.com>
2025-03-17 20:23:11 +0900

    🩹 Fix: change the country #16

--- src/utils/serverApi.ts
+++ src/utils/serverApi.ts
@@ -5,7 +5,7 @@ const BASE_URL = process.env.BASE_URL;
 export const fetchItemList = async () => {
   const versions = await fetchVersions();
   const response = await fetch(
-    `${BASE_URL}/cdn/${versions[0]}/data/en_US/item.json`
+    `${BASE_URL}/cdn/${versions[0]}/data/en_GB/item.json`
   );
 
   const data = await response.json();
@@ -18,7 +18,7 @@ export const fetchItemList = async () => {
 export const fetchChampionList = async () => {
   const versions = await fetchVersions();
   const response = await fetch(
-    `${BASE_URL}/cdn/${versions[0]}/data/en_US/champion.json`,
+    `${BASE_URL}/cdn/${versions[0]}/data/en_GB/champion.json`,
     {
       next: { revalidate: 86400 },
     }
@@ -34,7 +34,7 @@ export const fetchChampionList = async () => {
 export const fetchChampionDetails = async (id) => {
   const versions = await fetchVersions();
   const response = await fetch(
-    `${BASE_URL}/cdn/${versions[0]}/data/en_US/champion/${id}.json`,
+    `${BASE_URL}/cdn/${versions[0]}/data/en_GB/champion/${id}.json`,
     {
       cache: 'no-store',
     }
